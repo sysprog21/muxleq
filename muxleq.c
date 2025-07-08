73017f1384b946bbc4520b8a0999d688d97e17a9
Jim Huang <jserv@ccns.ncku.edu.tw>
2025-07-08 11:12:28 +0800

    Implement TCO-based dispatch loop for performance
    This commit overhauls the MUXLEQ virtual machine's main execution loop
    to improve performance and code structure. The previous while loop has
    been replaced with a set of mutually recursive functions that rely on
    guaranteed tail-call optimization (TCO).
    
    [ before ]
    $ echo "5000 ms bye" | time -p ./muxleq
    real 1.70
    
    [ after ]
    $ echo "5000 ms bye" | time -p ./muxleq
    real 1.05

--- muxleq.c
+++ muxleq.c
@@ -9,19 +9,24 @@
 #include <stdint.h>
 #include <stdio.h>
 
-/* branch predictor hints */
-#if defined(__GNUC__) || defined(__clang__)
-#define LIKELY(x) __builtin_expect(!!(x), 1)
-#define UNLIKELY(x) __builtin_expect(!!(x), 0)
+/* Tail-call optimization attribute */
+#if defined(__has_attribute) && __has_attribute(musttail)
+#define MUST_TAIL __attribute__((musttail))
 #else
-#define LIKELY(x) (x)
-#define UNLIKELY(x) (x)
+#define MUST_TAIL
+#endif
+
+/* Unused parameter attribute to suppress compiler warnings */
+#if defined(__GNUC__) || defined(__clang__)
+#define UNUSED __attribute__((unused))
+#else
+#define UNUSED
 #endif
 
 /* Define memory and instruction layout constants. */
 #define MEM_SIZE (1 << 15)
 #define MEM_MASK (MEM_SIZE - 1)
-#define IO_MARKER ((uint16_t) -1) /* More idiomatic way to express all 1s. */
+#define IO_MARKER ((uint16_t) -1)
 #define NEGATIVE_FLAG MEM_SIZE
 
 /* Instruction operand offsets. */
@@ -32,55 +37,99 @@ static uint16_t m[MEM_SIZE] = {
 #include "stage0.c"
 };
 
+/* Forward declarations for the mutually recursive VM functions. */
+static int dispatch(uint16_t pc,
+                    uint16_t addr_a,
+                    uint16_t addr_b,
+                    uint16_t addr_c);
+
+/* Fetch the next instruction's operands and tail-call dispatch. */
+#define FETCH_AND_DISPATCH(next_pc)                  \
+    do {                                             \
+        const uint16_t a = m[(next_pc) + A];         \
+        const uint16_t b = m[(next_pc) + B];         \
+        const uint16_t c = m[(next_pc) + C];         \
+        MUST_TAIL return dispatch(next_pc, a, b, c); \
+    } while (0)
+
+static int get(uint16_t pc,
+               UNUSED uint16_t addr_a,
+               uint16_t addr_b,
+               UNUSED uint16_t addr_c)
+{
+    const int input = getchar();
+    if (input == EOF)
+        return 0; /* Halt on End-of-File. */
+
+    m[addr_b] = (uint16_t) input;
+    FETCH_AND_DISPATCH(pc + INSN_SIZE);
+}
+
+static int put(uint16_t pc,
+               uint16_t addr_a,
+               UNUSED uint16_t addr_b,
+               UNUSED uint16_t addr_c)
+{
+    if (putchar(m[addr_a]) == EOF)
+        return 3; /* Halt on output error. */
+
+    FETCH_AND_DISPATCH(pc + INSN_SIZE);
+}
+
+static int mux(uint16_t pc, uint16_t addr_a, uint16_t addr_b, uint16_t addr_c)
+{
+    const uint16_t mask = m[addr_c & MEM_MASK];
+    m[addr_b] = (m[addr_a] & ~mask) | (m[addr_b] & mask);
+    FETCH_AND_DISPATCH(pc + INSN_SIZE);
+}
+
+static int subleq(uint16_t pc,
+                  uint16_t addr_a,
+                  uint16_t addr_b,
+                  uint16_t addr_c)
+{
+    const uint16_t result = m[addr_b] - m[addr_a];
+    m[addr_b] = result;
+    if ((result == 0) || (result & NEGATIVE_FLAG)) {
+        FETCH_AND_DISPATCH(addr_c); /* Branch */
+    } else {
+        FETCH_AND_DISPATCH(pc + INSN_SIZE);
+    }
+}
+
+static int dispatch(uint16_t pc,
+                    uint16_t addr_a,
+                    uint16_t addr_b,
+                    uint16_t addr_c)
+{
+    /* Halt if the program counter becomes negative. */
+    if ((pc & NEGATIVE_FLAG) != 0)
+        return 0;
+
+    /* Dispatch to the appropriate handler based on the operand values. */
+    if (addr_a == IO_MARKER) /* Input */
+        MUST_TAIL return get(pc, addr_a, addr_b, addr_c);
+
+    if (addr_b == IO_MARKER) /* Output */
+        MUST_TAIL return put(pc, addr_a, addr_b, addr_c);
+
+    if ((addr_c & NEGATIVE_FLAG) && (addr_c != IO_MARKER)) /* MUX */
+        MUST_TAIL return mux(pc, addr_a, addr_b, addr_c);
+
+    /* Default to SUBLEQ operation. */
+    MUST_TAIL return subleq(pc, addr_a, addr_b, addr_c);
+}
+
 int main(void)
 {
     /* Disable buffering for stdout to ensure immediate output */
     if (setvbuf(stdout, NULL, _IONBF, 0) != 0)
         return 1; /* Non-zero return indicates failure */
 
-    uint16_t pc = 0;
-    /* Loop until PC becomes negative. */
-    while (LIKELY((pc & NEGATIVE_FLAG) == 0)) {
-        /* Fetch instruction operands from memory. */
-        const uint16_t addr_a = m[pc + A];
-        const uint16_t addr_b = m[pc + B];
-        const uint16_t addr_c = m[pc + C];
-
-        /* Handle I/O as a special case. */
-        if (UNLIKELY(addr_a == IO_MARKER)) { /* Input */
-            const int input = getchar();
-            if (input == EOF)
-                break; /* Halt on End-of-File. */
-
-            m[addr_b] = (uint16_t) input;
-            pc += INSN_SIZE;
-            continue;
-        }
-
-        if (UNLIKELY(addr_b == IO_MARKER)) { /* Output */
-            if (putchar(m[addr_a]) == EOF)
-                return 3; /* Halt on output error. */
-
-            pc += INSN_SIZE;
-            continue;
-        }
-
-        /* Check for MUX operation, encoded in a negative 'c' address. */
-        if (UNLIKELY((addr_c & NEGATIVE_FLAG) && (addr_c != IO_MARKER))) {
-            const uint16_t mask = m[addr_c & MEM_MASK];
-            m[addr_b] = (m[addr_a] & ~mask) | (m[addr_b] & mask);
-            pc += INSN_SIZE;
-        } else {
-            /* Default to SUBLEQ operation. */
-            const uint16_t result = m[addr_b] - m[addr_a];
-            m[addr_b] = result;
-            if ((result == 0) || (result & NEGATIVE_FLAG)) {
-                pc = addr_c; /* Branch if result is zero or negative. */
-            } else {
-                pc += INSN_SIZE;
-            }
-        }
-    }
-
-    return 0;
+    /* Fetch first instruction and start execution by calling the dispatcher. */
+    const uint16_t pc = 0;
+    const uint16_t addr_a = m[pc + A];
+    const uint16_t addr_b = m[pc + B];
+    const uint16_t addr_c = m[pc + C];
+    return dispatch(pc, addr_a, addr_b, addr_c);
 }
